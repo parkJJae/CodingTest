e3c87a12e725ed352d9d8aa8ee7d723ef9d0bc2d
빡째 <wgpark0830@naver.com>
2026-05-12 17:05:16 +0900

    [Passed] 3의 배수 출력하기 (131ms, 10MB)

---
+++ Codetree/trail1/3의 배수 출력하기/print-multiple-of-number-three.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int i = 3;
+
+        while(i<=n){
+            System.out.print(i + " ");
+            i+=3;
+        }
+    }
+}
