8d54e84cf654eddc7f71598aaf8152c98d67a5b4
빡째 <wgpark0830@naver.com>
2026-05-14 17:48:43 +0900

    [Passed] 배수의 개수 (125ms, 10MB)

---
+++ Codetree/trail1/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for(int i=0; i<10; i++){
+            int n = sc.nextInt();
+            if(n%3==0){
+                cnt1++;
+            }
+            if(n%5==0){
+                cnt2++;
+            }
+        }
+        System.out.print(cnt1 + " " + cnt2);
+    }
+}
